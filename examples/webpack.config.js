ebf4e121d5f7a69ad6436683df714debbba3eba6
Cristian Oliveira <contato@cristianoliveira.com.br>
2018-02-17 22:44:40 +0100

    example: minimal setup

---
+++ examples/webpack.config.js
@@ -0,0 +1,27 @@
+const webpack = require('webpack');
+
+module.exports = {
+  entry: ['react-hot-loader/patch', './src/index.js'],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
+};
